9c8cdde9550cca8694b9f3615fc81908ef167c86
Aleksey Sidorov <ealesid@alekseys-mac.local>
2018-12-29 13:44:42 +0300

    delete row

--- jbplanner/Controllers/List/TaskListController.swift
+++ jbplanner/Controllers/List/TaskListController.swift
@@ -11,10 +11,10 @@ import UIKit
 
 class TaskListController: UITableViewController {
     
+    let db = Db()
+    
     let dateFormatter = DateFormatter()
-    
-    var context: NSManagedObjectContext! // контекст для связи объектов с БД
-    
+        
     var taskList:[Task]!
     
     // временный массив данных
@@ -30,17 +30,10 @@ class TaskListController: UITableViewController {
         dateFormatter.dateStyle = .short
         dateFormatter.timeStyle = .none
         
-        // используем AppDelegate для получения доступа к контексту
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            fatalError("appDelegate error")
-        }
+       
+//        db.initData()
         
-        // получаем контекст из persistentContainer
-        context = appDelegate.persistentContainer.viewContext
-        
-//        initData()
-        
-        taskList = getAllTasks()
+        taskList = db.getAllTasks()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -115,74 +108,22 @@ class TaskListController: UITableViewController {
         return 60
     }
     
-    func addCategory(name: String) -> Category {
-        let category = Category(context: context)
-        category.name = name
-        do {
-            try context.save()
-        } catch let error as NSError {
-            print("Category could not save : \(error)")
-        }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
-        return category
+        if editingStyle == .delete {
+            
+            db.deleteTask(task: taskList[indexPath.row]) //удалить задачу из БД
+            
+            // удалить саму строку и объект из коллекции
+            taskList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .top)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appripriate class, insert it into the array, and add a new row to the tableView
+            
+        }
     }
     
-    func addPriority(name: String, index: Int32) -> Priority {
-        let priority = Priority(context: context)
-        priority.name = name
-        priority.index = index
-        do {
-            try context.save()
-        } catch let error as NSError {
-            print("Priority could not save : \(error)")
-        }
-        
-        return priority
-    }
     
-    func addTask(name: String, completed: Bool, deadline: Date?, info: String?, category: Category?, priority: Priority?) -> Task {
-        let task = Task(context: context)
-        task.name = name
-        task.completed = completed
-        task.deadline = deadline
-        task.info = info
-        task.category = category
-        task.priority = priority
-        
-        do {
-            try context.save()
-        } catch let error as NSError {
-            print("Task could not save: \(error)")
-        }
-        
-        return task
-    }
-    
-    func initData() {
-        let cat1 = addCategory(name: "Sport")
-        let cat2 = addCategory(name: "Family")
-        let cat3 = addCategory(name: "Vacation")
-        
-        let priority1 = addPriority(name: "Low", index: 1)
-        let priority2 = addPriority(name: "Meduim", index: 2)
-        let priority3 = addPriority(name: "High", index: 3)
-        
-        let task1 = addTask(name: "Visit swimpool", completed: false, deadline: Date(), info: "additional info", category: cat1, priority: priority2)
-        let task2 = addTask(name: "Weekend with family", completed: false, deadline: Date().rewindDays(2), info: "additional info", category: cat2, priority: priority1)
-    }
-    
-    func getAllTasks() -> [Task] {
-        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
-        let list:[Task]
-        
-        do {
-            list = try context.fetch(fetchRequest)
-        } catch {
-            fatalError("Task fetch failed!")
-        }
-        
-        return list
-    }
     
     //название для каждой секции
 //    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
